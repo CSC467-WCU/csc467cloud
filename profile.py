5b3411617c8fecd5890b80e29eefdcd1e1a8189d
Linh B Ngo <lngo@wcupa.edu>
2019-12-19 10:00:35 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -4,14 +4,30 @@ import geni.rspec.igext as IG
 
 pc = portal.Context()
 request = pc.makeRequestRSpec()
-
-tourDescription = "A compute node with Docker installed on Ubuntu 18.04" 
+tourDescription = \
+"""
+This profile provides the template for a Hortonworks cluster with one 
+namenode and seven datanode.
+"""
 tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 request.addTour(tour)
 
-node = request.RawPC("head")
-node.routable_control_ip = "true" 
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh")) 
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
+
+for i in range(8):
+  if i == 0:
+    node = request.XenVM("namenode")
+  else:
+    node = request.XenVM("datanode-" + str(i))
+  node.cores = 4
+  node.ram = 8192
+  node.routable_control_ip = "true"  
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)
+
 pc.printRequestRSpec(request)
