6c1e73892a90d55db3b518f0c58f9ccdd64b542c
Linh B Ngo <lngo@wcupa.edu>
2019-12-19 09:22:44 -0500

    Create install_nfs_server.sh

---
+++ install_nfs_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "install NFS server"
+set -x
+
+sudo chown nobody:nogroup /opt
+sudo chmod -R a+rx /opt
+
+#################
+sudo apt-get update
+sudo apt-get install -y nfs-kernel-server
+
+nodes=$(($1 + 1))
+for i in $(seq 2 $nodes)
+do
+  echo "/opt 192.168.1.$i(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+done
+
+sudo systemctl restart nfs-kernel-server
