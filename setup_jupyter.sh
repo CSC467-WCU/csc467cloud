7fd657e54b702635c80377ab2497879c3d25f357
Linh B Ngo <lngo@wcupa.edu>
2020-01-06 18:08:04 -0500

    Update setup_jupyter.sh

--- setup_jupyter.sh
+++ setup_jupyter.sh
@@ -4,4 +4,6 @@ wget --no-check-certificate https://repo.continuum.io/miniconda/Miniconda3-lates
 bash Miniconda3-latest-Linux-x86_64.sh -b -p /opt/anaconda3
 echo "PATH=/opt/anaconda3/bin:$PATH" | sudo tee -a /etc/environment
 source /etc/environment
-conda install -c conda-forge notebook
+conda install -y -c conda-forge notebook
+conda install -y -c conda-forge feedparser
+conda install -y -c anaconda beautifulsoup4
