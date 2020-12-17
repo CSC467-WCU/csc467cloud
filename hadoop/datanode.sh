732743ed7723dda0ca01a03dbf34a0501ada6b73
Linh B Ngo <lngo@wcupa.edu>
2020-12-16 21:47:35 -0500

    Create datanode.sh

---
+++ hadoop/datanode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export JAVA_HOME="/software/jdk8u275-b01/"
+export HADOOP_HOME="/software/hadoop-3.3.0"
+export HADOOP_CONF_DIR="/software/hadoop/config"
+
+DATANODE=`hostname -f`
+
+echo $DATANODE >> workers
+
+time ${HADOOP_HOME}/sbin/hadoop-daemons.sh --config ${HADOOP_CONF_DIR} start datanode
+time ${HADOOP_HOME}/sbin/yarn-daemons.sh --config ${HADOOP_CONF_DIR} start nodemanager
+
