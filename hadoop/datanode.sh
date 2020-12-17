014ab49eda46afeef2479731718bd0364b103e6b
Linh B Ngo <lngo@wcupa.edu>
2020-12-16 21:53:13 -0500

    Update datanode.sh

--- hadoop/datanode.sh
+++ hadoop/datanode.sh
@@ -6,7 +6,7 @@ export HADOOP_CONF_DIR="/software/hadoop/config"
 
 DATANODE=`hostname -f`
 
-echo $DATANODE >> workers
+echo $DATANODE >> ${HADOOP_CONF_DIR}/workers
 
 time ${HADOOP_HOME}/sbin/hadoop-daemons.sh --config ${HADOOP_CONF_DIR} start datanode
 time ${HADOOP_HOME}/sbin/yarn-daemons.sh --config ${HADOOP_CONF_DIR} start nodemanager
